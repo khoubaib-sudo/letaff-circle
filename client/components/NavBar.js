da5c1a6d390f493f82ec7557c23de4fea2385887
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-02-01 15:51:46 +0100

    add login and regitser

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
     <div className='mx-auto flex justify-center '>
         <div className="container flex flex-col lg:flex-row py-2 justify-between items-center">
-            <img src="/assets/logo.png" alt="logo" />
+            <img src="/assets/logo.png" alt="logo" href={'/register'}/>
             <div className="flex items-center gap-4">
-                <p className='text-base font-medium cursor-pointer'>Sign In</p>
-                <p className='text-base font-medium capitalize bg-purple-500 border border-none cursor-pointer btn btn-sm'>Create Free Account</p>
-             
+                <p className='text-base font-medium cursor-pointer'><Link href={'/login'}>Sign In</Link></p>
+                <p className='text-base font-medium capitalize bg-purple-500 border border-none cursor-pointer btn btn-sm'>
+                  <Link  href={'/register'}>Create Free Account</Link>
+                  </p>
             </div>
         </div>
     </div>
   )
 }
 
-export default NavBar
+export default NavBar;
--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar/>
       <Hero/>
       <Services/>
       <PopularClass/>
