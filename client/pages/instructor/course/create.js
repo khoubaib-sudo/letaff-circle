e2127bd995e5a724b497b70b6947851108c67754
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-03-21 00:10:15 +0100

    Update create.js

--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -27,25 +27,27 @@ const CourseCreate = () => {
     console.log(values);
   };
 
-  const CourseCreateForm = () => {
-    <form onSubmit={handleSubmit} className="form-group">
+  const courseCreateForm = () => {
+    return (
+    <form onSubmit={handleSubmit} >
       <div className="form-group">
         <input
           type="text"
           name="name"
-          className="form-control"
+          className="w-full px-4 py-2 leading-tight text-gray-700 bg-gray-100 border border-gray-300 rounded-md appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
           placeholder="Name"
           value={values.name}
           onChange={handleSubmit}
         />
       </div>
-    </form>;
+    </form>
+    );
   };
 
   return (
     <InstructorRoute>
       <h1 className="text-center">Create Course</h1>
-      <div className="pt-3 pb-3">{CourseCreateForm()}</div>
+      <div className="pt-3 pb-3">{courseCreateForm()}</div>
     </InstructorRoute>
   );
 };
