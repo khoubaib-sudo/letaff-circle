3632f820b474ab5beab7be5dc57cba1471402038
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-04-28 11:52:40 +0200

    IS enrolled middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,5 +1,6 @@
 const jwt = require('express-jwt');
 import User from "../models/user"
+import Course from "../models/course";
 
 const getToken = (req) => {
   if (req.cookies && req.cookies.token) {
@@ -28,3 +29,25 @@ export const isInstructor = async (req, res, next) => {
     console.log(err)
   }
 }
+
+
+export const isEnrolled = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const course = await Course.findOne({ slug: req.params.slug }).exec();
+
+    // check if course id is found in user courses array
+    let ids = [];
+    for (let i = 0; i < user.courses.length; i++) {
+      ids.push(user.courses[i].toString());
+    }
+
+    if (!ids.includes(course._id.toString())) {
+      res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
